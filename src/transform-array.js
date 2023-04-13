f57aa2d80cf5f45efaea2f760f68bd9a2a673853
irinazhibul <zhibulirina@gmail.com>
2023-04-14 03:35:05 +0400

    feat: add solution for transform array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,10 +1,10 @@
-const { NotImplementedError } = require('../extensions/index.js');
+/*const { NotImplementedError } = require('../extensions/index.js');*/
 
 /**
  * Create transformed array based on the control sequences that original
  * array contains
  * 
- * @param {Array} arr initial array
+ * @param {Array} clonedArray initial array
  * @returns {Array} transformed array
  * 
  * @example
@@ -13,10 +13,60 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+  if (!Array.isArray(arr)) throw new Error(`\'arr\' parameter must be an instance of the Array!`);
+  let clonedArray = arr.slice(0);
+  for (let i = 0; i < clonedArray.length; i++) {
+    if (clonedArray[i] === '--double-next') {
+      if (i+1 === clonedArray.length) {
+        clonedArray.pop()
+        return clonedArray;
+      }
+      if (i+2 === '--discard-prev') {
+        clonedArray.splice(i, 1) 
+        clonedArray.splice(i+2, 1) 
+        continue
+      }
+      if (i+2 === '--double-prev') {
+        clonedArray[i+2] = clonedArray[i+1]
+      }
+      clonedArray[i] = clonedArray[i+1]
+    }
+    if (clonedArray[i] === '--discard-next') {
+      if (i+1 === clonedArray.length) {
+        clonedArray.pop()
+        return clonedArray;
+      }
+      if (i+2 === '--discard-prev' || '--double-prev') {
+        clonedArray.splice(i, 3)
+      i--; 
+      continue;
+      }
+      clonedArray.splice(i, 2)
+      i--;
+    }
+    if (clonedArray[i] === '--discard-prev') {
+      if (i === 0) {
+        clonedArray.shift()
+        continue;
+      }
+      clonedArray.splice(i-1, 2)
+      i = i - 2;
+    }
+    if (clonedArray[i] === '--double-prev') {
+      if (i === 0) {
+        clonedArray.shift()
+        continue;
+      }
+      clonedArray[i] = clonedArray[i-1]
+    }
+  }
+  return clonedArray;
 }
+
+transform([1, 2, 3, '--double-next', 1337, '--double-prev', 4, 5])
+console.log(transform([1, 2, 3, '--double-next', 1337, '--double-prev', 4, 5]));
+
 
 module.exports = {
   transform
